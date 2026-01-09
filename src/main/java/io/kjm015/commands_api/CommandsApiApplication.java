f5ce1eb279fba95cd059fdf305b54299be5046a1
kjm015 <kjm015@yahoo.com>
2026-01-09 16:28:13 -0600

    change default local host port

--- src/main/java/io/kjm015/commands_api/CommandsApiApplication.java
+++ src/main/java/io/kjm015/commands_api/CommandsApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommandsApiApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(CommandsApiApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(CommandsApiApplication.class, args);
+    }
 }
